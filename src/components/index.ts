b61819f5b6ead696a353844f079f2ab48aec3220
Joel Alves Pereira <atejap05@yahoo.com.br>
2024-02-24 15:21:05 -0300

    implementa Input e layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { Button } from "./Button/Button";
 export { Text } from "./Text/Text";
+export { Input } from "./Input/Input";
+export { Box } from "./Layout/Box/Box";
+export { Stack } from "./Layout/Stack/Stack";
